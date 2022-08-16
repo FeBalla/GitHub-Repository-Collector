9f9b52e01a044be6ff10b951e83bb476d97cfe99
FeBalla <fballadares@uc.cl>
2022-08-16 15:54:13 -0400

    feat: support for different OS; fix: last commit date according to local time; (general refactor)

--- logger.py
+++ logger.py
@@ -1,24 +1,26 @@
-from datetime import datetime
 import os
+from datetime import datetime
 
 
 class Logger:
+  '''Handle execution logs (repositories list, runtime messages, etc.)'''
+
   def __init__(self, folder_name="logs", encoding="utf-8"):
     self.folder_name = folder_name
 
     if not os.path.exists(self.folder_name):
       os.makedirs(self.folder_name)
 
-    self.repositories_data_file = open(f"{self.folder_name}/repos.txt", "w", encoding=encoding)
+    self.repositories_data_file = open(os.path.join(self.folder_name, "repos.txt"), "w", encoding=encoding)
     print("Repositories expected to clone:", file=self.repositories_data_file)
 
-    self.runtime_logs_file = open(f"{self.folder_name}/runtime.txt", "w", encoding=encoding)
+    self.runtime_logs_file = open(os.path.join(self.folder_name, "runtime.txt"), "w", encoding=encoding)
     print("Runtime logs:", file=self.runtime_logs_file)
 
   def save_repositories_data(self, repositories_data):
     '''Writes a log-file with all the information of the repositories that will be cloned'''
-    repositories_data = sorted(repositories_data, key=lambda repo: repo['last_commit_date'])
-    
+    repositories_data = sorted(repositories_data, key=lambda repo: repo["last_commit_date"])
+
     for repo in repositories_data:
       print(repo["name"], end=" ", file=self.repositories_data_file)
       print(repo["last_commit_sha"], end=" ", file=self.repositories_data_file)
@@ -29,15 +31,15 @@ class Logger:
     os.fsync(self.repositories_data_file.fileno())
 
   def write_runtime_log(self, msg):
-    '''Writes a runtime-log (msg) in the log-file with the current time'''
+    '''Writes a runtime-log message in the log-file with the current time'''
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
 
-    print(f"({current_time}){msg}", file=self.runtime_logs_file)
+    print(f"({current_time}) {msg}", file=self.runtime_logs_file)
     self.runtime_logs_file.flush()
     os.fsync(self.runtime_logs_file.fileno())
 
   def finalize(self):
-    '''Ends the logger handler, saving and closing the used files'''
+    '''Ends the logger handler instance, saving and closing the used files'''
     self.runtime_logs_file.close()
     self.repositories_data_file.close()
--- main.py
+++ main.py
@@ -1,25 +1,21 @@
-from github_api_handler import get_assignment_repos_names, get_repository_information
+import os
 from dotenv import dotenv_values
 from git import Repo, rmtree
+from utilities import get_assignment_repos_names, get_repository_information
 from logger import Logger
-import os
 
 config = dotenv_values(".env")
-
-BASE_SAVE_DIR = f"../{config['PREFIX']}"
-PER_PAGE = 100
-PAGES    = 3
-
+SAVE_DIR = os.path.join("..", config["PREFIX"])
 
 if __name__ == "__main__":
-  cloned = 0
-  failed = 0
+  CLONED = 0
+  FAILED = 0
 
   print("(1/6) Initializing")
   logger = Logger()
 
-  print(f"(2/6) Getting repositories names from {config['ORGA']} with prefix: {config['PREFIX']}")
-  repo_names = get_assignment_repos_names(config["PREFIX"], PER_PAGE, PAGES)
+  print(f"(2/6) Getting repositories list from {config['ORGA']} with prefix: {config['PREFIX']}")
+  repo_names = get_assignment_repos_names(config["PREFIX"], int(config["PER_PAGE"]), int(config["PAGES"]))
 
   print("(3/6) Getting repositories data")
   repositories_data = [get_repository_information(name, config["PREFIX"]) for name in repo_names]
@@ -27,24 +23,24 @@ if __name__ == "__main__":
   print("(4/6) Saving repositories data")
   logger.save_repositories_data(repositories_data)
 
-  if not os.path.exists(BASE_SAVE_DIR):
-    os.makedirs(BASE_SAVE_DIR)
+  if not os.path.exists(SAVE_DIR):
+    os.makedirs(SAVE_DIR)
 
   print("(5/6) Cloning repositories")
   for repo in repositories_data:
     try:
-      cloned_repo = Repo.clone_from(repo["clone_url"], f"{BASE_SAVE_DIR}/{repo['name']}", no_checkout=True)
+      cloned_repo = Repo.clone_from(repo["clone_url"], os.path.join(SAVE_DIR, repo["name"]), no_checkout=True)
       cloned_repo.git.checkout(repo["last_commit_sha"])
-      rmtree(f"{BASE_SAVE_DIR}/{repo['name']}/.git")
+      rmtree(os.path.join(SAVE_DIR, repo["name"], ".git"))
 
       logger.write_runtime_log(f"Cloned succesfully: {repo['name']}")
-      cloned += 1
-    except:
+      CLONED += 1
+    except Exception:
       logger.write_runtime_log(f"Couldn't clone: {repo['name']}")
-      failed += 1
+      FAILED += 1
 
   logger.finalize()
 
   print("(6/6) Process completed")
-  print(f"- Cloned: {cloned}")
-  print(f"- Failed: {failed}")
+  print(f"- Cloned: {CLONED}")
+  print(f"- Failed: {FAILED}")
